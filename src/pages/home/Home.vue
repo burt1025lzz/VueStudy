7f8378ac60a5ecf585f2c1c088c7f9d562d8f5fb
liuzhaozhi <1765805596@qq.com>
2020-11-08 19:09:08 +0800

    周末游组件编写

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -8,6 +8,8 @@
     <home-icons></home-icons>
     <!--    热销推荐组件-->
     <home-recommend></home-recommend>
+    <!--    周末游组件-->
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -16,6 +18,7 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 
 export default {
   name: 'Home',
@@ -23,7 +26,8 @@ export default {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
