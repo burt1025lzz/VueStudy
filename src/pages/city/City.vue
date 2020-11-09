f97ae2092195b695de1a003dc5c9735c7e0e5060
liuzhaozhi <1765805596@qq.com>
2020-11-09 21:46:42 +0800

    城市列表页搜索组件样式编写

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,21 @@
 <template>
-  <!--  城市列表头部组件-->
-  <city-header></city-header>
+  <div>
+    <!--    城市列表头部组件-->
+    <city-header></city-header>
+    <!--    城市列表搜索组件-->
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search">
+    <label>
+      <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~css/varibles.styl"
+.search
+  background $bgColor
+  padding 0 .1rem
+  height .72rem
+
+  .search-input
+    // 这里若想使用padding
+    // 要配合使用属性box-sizing: border-box
+    box-sizing border-box
+    padding 0 .1rem
+    width 100%
+    height .62rem
+    line-height .62rem
+    text-align center
+    border-radius .06rem
+    color $darkTextColor
+</style>
